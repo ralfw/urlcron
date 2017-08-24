b2d205a56ce969203f750ff608ec8641c458d539
Ralf Westphal <info@ralfw.de>
2017-08-24 18:14:17 +0200

    /info läuft lokal

---
+++ src/urlcron/urlcron.service/ServicePortal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using servicehost.contract;
+
+namespace urlcron.service
+{
+    [Service]
+    public class ServicePortal
+    {
+        [EntryPoint(HttpMethods.Get, "/info")]
+        public string Info() {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
---
+++ src/urlcron/urlcron/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Unix;
+using Mono.Unix.Native;
+using servicehost;
+
+namespace urlcron
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var endpoint = new Uri(args[0]);
+            
+            using (var servicehost = new ServiceHost())
+            {
+                servicehost.Start(endpoint);
+                Console.WriteLine("Service host running on {0}...", endpoint.ToString());
+
+                if (Is_running_on_Mono) {
+                    Console.WriteLine("Ctrl-C to stop service host");
+                    UnixSignal.WaitAny(UnixTerminationSignals);
+                }
+                else {
+                    Console.WriteLine("ENTER to stop service host");
+                    Console.ReadLine();
+                }
+
+                Console.WriteLine("Stopping service host");
+                servicehost.Stop();
+            }
+        }
+
+        private static bool Is_running_on_Mono => Type.GetType("Mono.Runtime") != null;
+
+        private static UnixSignal[] UnixTerminationSignals =>  new[] {
+            new UnixSignal(Signum.SIGINT),
+            new UnixSignal(Signum.SIGTERM),
+            new UnixSignal(Signum.SIGQUIT),
+            new UnixSignal(Signum.SIGHUP)
+        };
+    }
+}
