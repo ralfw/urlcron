1d4f6e8ca0a33b9fdc7b508aec536d033c1020b5
Ralf Westphal <info@ralfw.de>
2017-08-30 07:38:55 +0200

    Extracted Server class

--- src/urlcron/urlcron/Program.cs
+++ src/urlcron/urlcron/Program.cs
@@ -1,43 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using Mono.Unix;
-using Mono.Unix.Native;
-using servicehost;
+using Nancy;
 
 namespace urlcron
 {
     internal class Program
     {
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) {
             var endpoint = new Uri(args[0]);
-            
-            using (var servicehost = new ServiceHost())
-            {
-                servicehost.Start(endpoint);
-                Console.WriteLine("Service host running on {0}...", endpoint.ToString());
-
-                if (Is_running_on_Mono) {
-                    Console.WriteLine("Ctrl-C to stop service host");
-                    UnixSignal.WaitAny(UnixTerminationSignals);
-                }
-                else {
-                    Console.WriteLine("ENTER to stop service host");
-                    Console.ReadLine();
-                }
-
-                Console.WriteLine("Stopping service host");
-                servicehost.Stop();
-            }
+            Server.Run(endpoint);
         }
-
-        private static bool Is_running_on_Mono => Type.GetType("Mono.Runtime") != null;
-
-        private static UnixSignal[] UnixTerminationSignals =>  new[] {
-            new UnixSignal(Signum.SIGINT),
-            new UnixSignal(Signum.SIGTERM),
-            new UnixSignal(Signum.SIGQUIT),
-            new UnixSignal(Signum.SIGHUP)
-        };
     }
 }
---
+++ src/urlcron/urlcron/Server.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Mono.Unix;
+using Mono.Unix.Native;
+using servicehost;
+
+namespace urlcron
+{
+    class Server 
+    {
+        public static void Run(Uri endpoint) {
+            using (var servicehost = new ServiceHost()) {
+                servicehost.Start(endpoint);
+                Console.WriteLine("Service host running on {0}...", endpoint.ToString());
+
+                if (Is_running_on_Mono) {
+                    Console.WriteLine("Ctrl-C to stop service host");
+                    UnixSignal.WaitAny(UnixTerminationSignals);
+                }
+                else {
+                    Console.WriteLine("ENTER to stop service host");
+                    Console.ReadLine();
+                }
+
+                Console.WriteLine("Stopping service host");
+                servicehost.Stop();
+            }
+        } 
+        
+        private static bool Is_running_on_Mono => Type.GetType("Mono.Runtime") != null;
+
+        private static UnixSignal[] UnixTerminationSignals =>  new[] {
+            new UnixSignal(Signum.SIGINT),
+            new UnixSignal(Signum.SIGTERM),
+            new UnixSignal(Signum.SIGQUIT),
+            new UnixSignal(Signum.SIGHUP)
+        };
+    }
+}
